6f39be1873c2298d83dc4608db0c843c933562a1
AhmadOsaili <ahmadosaili@hotmail.com>
2021-03-17 12:37:40 -0500

    commit

---
+++ AhmadOsaili/WebContent/study.js
@@ -0,0 +1,13 @@
+
+window.onload = function() {
+
+
+}
+const name = 'Ahmad';
+const age = 28;
+const numbers = [1,2,3,4];
+
+console.log(`My name is ${name} and I am ${age}`);
+console.log(name.split(''));
+console.log(numbers);
+
